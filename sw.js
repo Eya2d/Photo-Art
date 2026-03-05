ddfde928401b2e364b28d179329f02b9f01cb8cd
Eyad <eyadscript@gmail.com>
2026-03-05 07:17:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,9 +3,8 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+  self.clients.claim();
 });
